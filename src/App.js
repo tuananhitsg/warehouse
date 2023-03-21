dd9fdda0990b7c52f2a652577514b2903b543949
sieubom123w <tuananhitsg@gmail.com>
2023-03-21 21:41:22 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,45 +1,46 @@
 import React, { useEffect, useState } from "react";
 import { lazy, Suspense } from "react";
-import { Route, Routes,useNavigate } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Loading from "./components/basic/loading";
-import { useDispatch } from 'react-redux';
+import { useDispatch } from "react-redux";
 import "./App.css";
 import authService from "./service/auth.service";
-import {setUser} from "./redux/userSlice";
+import { setUser } from "./redux/userSlice";
 
-const ChangePassword = lazy(() => import("./components/account/change_password"));
+const ChangePassword = lazy(() =>
+  import("./components/account/change_password")
+);
 const ForgotPassword = lazy(() => import("./components/account/forgot"));
 const LoginForm = lazy(() => import("./components/account/loginForm"));
 const HomePage = lazy(() => import("./pages/homePage"));
-
 const App = () => {
-    const dispatch=useDispatch();
-    const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    // useEffect(() => {
-    //     const userInLocalStorage = authService.getUser();
-    //     if (userInLocalStorage) {
-    //         dispatch(setUser(userInLocalStorage));
-    //         navigate("/trang-chu");
-    //     } else {
-    //         navigate("/dang-nhap");
-    //     }
-    // },[]);
+  // useEffect(() => {
+  //     const userInLocalStorage = authService.getUser();
+  //     if (userInLocalStorage) {
+  //         dispatch(setUser(userInLocalStorage));
+  //         navigate("/trang-chu");
+  //     } else {
+  //         navigate("/dang-nhap");
+  //     }
+  // },[]);
 
-    return (
-        <div className="App">
-            <Suspense fallback={<Loading />}>
-                <Routes>
-                    <Route path="dang-nhap" element={<LoginForm />} />
-                    <Route path="" element={<LoginForm />} />
-                    <Route path="quen-mat-khau" element={<ForgotPassword />} />
-                    <Route path="doi-mat-khau" element={<ChangePassword />} />
-                    {/* <Route path="quan-ly/*" element={<Management />} /> */}
-                    <Route path="trang-chu" element={<HomePage />} />
-                </Routes>
-            </Suspense>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="dang-nhap" element={<LoginForm />} />
+          <Route path="" element={<LoginForm />} />
+          <Route path="quen-mat-khau" element={<ForgotPassword />} />
+          <Route path="doi-mat-khau" element={<ChangePassword />} />
+          {/* <Route path="quan-ly/*" element={<Management />} /> */}
+          <Route path="trang-chu" element={<HomePage />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
 };
 
 export default App;
--- src/api/employeeApi.js
+++ src/api/employeeApi.js
@@ -5,10 +5,18 @@ const employeeApi = {
         const url = "/user/get-all";
         return axiosApi.get(url);
     },
+    getEmployeesById: (id) => {
+        const url = `/user/get-by/${id}`;
+        return axiosApi.get(url);
+    },
     addEmployee: (params) => {
         const url = "/register";
         return axiosApi.post(url, params);
     },
+    updateEmployee: (id, params) => {
+        const url = `/user/update/${id}`;
+        return axiosApi.put(url, params);
+    }
     
 }
 export default employeeApi;
