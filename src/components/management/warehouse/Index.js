49ed2bb8ace5e627654b6b7cb36b8e29410f02f1
sieubom123w <tuananhitsg@gmail.com>
2023-04-08 20:50:38 +0700

    feat: fix file name

--- src/components/management/warehouse/Index.js
+++ src/components/management/warehouse/Index.js
@@ -4,17 +4,15 @@ import { Input, Col, Row, Typography, Button, Modal } from "antd";
 import { SearchOutlined, UserAddOutlined } from "@ant-design/icons";
 import WareHouseTable from "./Table";
 const { Title, Text } = Typography;
-
-const IndexWarehouse = ({setTab}) => {
+const IndexWarehouse = ({ setTab }) => {
   return (
     <div className="site-card-wrapper">
       <Title level={5} style={{ marginBottom: "1rem" }}>
         Nhà kho
       </Title>
-
       <Row>
         <Col span={24}>
-          <WareHouseTable setTab={setTab}/>
+          <WareHouseTable setTab={setTab} />
         </Col>
       </Row>
     </div>
